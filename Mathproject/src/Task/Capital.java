826e64bf66575b1f654fa8bc98c765baf498502b
ADMIN <ADMIN@QA-NINJA-1>
2021-09-07 22:51:34 -0400

    First commit

---
+++ Mathproject/src/Task/Capital.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class Capital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+toyota();
+	}
+public static void toyota() {
+System.out.println("i am so dumb");
+
+}
+}
